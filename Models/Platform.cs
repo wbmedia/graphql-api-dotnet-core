4d428b497cb901a197749c4ac565e91c48d2a4e1
Antonio Nicasio <webmediadigital@gmail.com>
2021-11-04 17:48:44 -0700

    Add Description to our query
    Add Description to our query

--- Models/Platform.cs
+++ Models/Platform.cs
@@ -1,14 +1,17 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using HotChocolate;
 
 namespace CommanderGQL.Models
 {
+    [GraphQLDescription("Represent any software or service that has a command line interface.")]
     public class Platform
     {
         [Key]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [GraphQLDescription("Represents a purchased valid, licensekey for a product platform")]
         public string LicenseKey { get; set; }
         public ICollection<Command> Commands { get; set; } = new List<Command>();
     }
